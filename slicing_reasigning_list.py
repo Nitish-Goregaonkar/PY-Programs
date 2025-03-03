1d59544edea7b91ca79af5e51989df8dde8e91f0
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-03 23:06:15 +0530

    minor change

--- slicing_reasigning_list.py
+++ slicing_reasigning_list.py
@@ -3,4 +3,4 @@ my_list = [1, 2, 3, 4, 5]
  
 # Replace elements at index 1 through 3 with new elements  
 my_list[1:4] = ['a', 'b', 'c'] 
-print(my_list) # Output: [1, 'a', 'b', 'c', 5]
+print(my_list) # op [1, 'a', 'b', 'c', 5]
