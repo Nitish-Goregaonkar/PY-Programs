6652367b3813b44daaaaaeb587a874e145d24c05
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-20 21:04:54 +0530

    pyexp

--- color.py
+++ color.py
@@ -2,3 +2,9 @@ from termcolor import colored
 print(colored("This is red text", "red"))
 print(colored("This is green text", "green"))
 print(colored("This is yellow text", "yellow"))
+
+
+# Example: Multiplication table
+for i in range(1, 4):  # Outer loop
+    for j in range(1, 4):  # Inner loop
+        print(f"{i} * {j} = {i * j}")
