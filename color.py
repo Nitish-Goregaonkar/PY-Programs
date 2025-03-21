55ee866abb5222da4f12d64870baadeb70d4b146
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-21 10:14:50 +0530

    pyexp

--- color.py
+++ color.py
@@ -8,3 +8,8 @@ print(colored("This is yellow text", "yellow"))
 for i in range(1, 4):  # Outer loop
     for j in range(1, 4):  # Inner loop
         print(f"{i} * {j} = {i * j}")
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+even_no=[num for num in numbers if num %2==0]
+print("even numbers:", even_no)
