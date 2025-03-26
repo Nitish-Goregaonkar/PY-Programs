54a7cb2ec9163554de8a60e47d42250f0a834fce
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-26 11:54:11 +0530

    pyexp

--- loop.py
+++ loop.py
@@ -39,3 +39,4 @@ def pascals_triangle(n):
      for i in range(2, n):
            for j in range(1, i):
                  triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+     for row in triangle:
