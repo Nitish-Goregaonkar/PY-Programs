9f33aee76180088b9efe288e4ef55f4a27672ca7
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-30 21:44:20 +0530

    exp8

---
+++ exp10_question.py
@@ -0,0 +1,8 @@
+# Book class with user input
+class Book:
+    def __init__(self, name, author, price):
+        self.name = name
+        self.author = author
+        self.price = price
+    
+    
