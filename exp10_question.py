7d303ded2568307f56e63491ede620f8126f129d
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-30 21:44:37 +0530

    exp8

--- exp10_question.py
+++ exp10_question.py
@@ -5,4 +5,14 @@ class Book:
         self.author = author
         self.price = price
     
-    
+    def display_details(self):
+        print(f"Book Name: {self.name}, Author: {self.author}, Price: {self.price}")
+
+name = input("Enter book name: ")
+author = input("Enter author name: ")
+price = float(input("Enter book price: "))
+
+book = Book(name, author, price)
+book.display_details()
+
+
