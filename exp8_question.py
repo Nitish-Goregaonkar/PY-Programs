db65c9e51acdb0e33d88dca8dbb2e5171ca7737c
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-30 21:42:08 +0530

    exp8

--- exp8_question.py
+++ exp8_question.py
@@ -9,4 +9,19 @@ class StudentManagementSystem:
         self.students[student_id] = {"name": name, "age": age, "grades": grades}
         print("Student added!")
 
-    
+    def update_student(self, student_id, name=None, age=None, grades=None):
+        if student_id in self.students:
+            if name: 
+                self.students[student_id]["name"] = name
+            if age: 
+                self.students[student_id]["age"] = age
+            if grades:
+                if len(grades) != 3:
+                    print("Please enter exactly 3 grades.")
+                    return
+                self.students[student_id]["grades"] = grades
+            print("Student updated!")
+        else:
+            print("Student not found!")
+
+   
