c254a3f45131cd94c4bc67bb7526a247e0d36263
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-31 21:58:14 +0530

    comments

--- exp10_question.py
+++ exp10_question.py
@@ -19,10 +19,11 @@ book.display_details()
 
 # BankAccount class with user input and conditional balance assignment
 class BankAccount:
+    #creating the constructor
     def __init__(self, account_number, balance=1000):
         self.account_number = account_number
         self.balance = max(balance, 100)
-    
+    #displaying the details 
     def display_account(self):
         print(f"Account Number: {self.account_number}, Balance: {self.balance}")
 
