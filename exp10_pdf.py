079ae065d981f7ceaed29f8249ee683f08e4204e
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-06 21:26:04 +0530

    exp11

---
+++ exp10_pdf.py
@@ -0,0 +1,13 @@
+#default constructor 
+class Person: 
+def  init (self):
+    # Default values for attributes 
+ self.name = "Unknown" 
+self.age = 0 
+ 
+def show_info(self): 
+ print(f"Name: {self.name}, Age: {self.age}") 
+ 
+# Creating an object with the default constructor  
+person1 = Person() 
+person1.show_info()
