2262cb56db6130315c0fd162e07a67b9cb6c2407
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-06 21:23:06 +0530

    exp11

--- exp11_pdf.py
+++ exp11_pdf.py
@@ -1,10 +1,21 @@
 #Example of Method Overloading Using Default Arguments and *args
-class MathOperations: 
-    # Method to add numbers 
-    def add(self, a, b=0, c=0): 
-        return a + b + c 
+# class MathOperations: 
+#     # Method to add numbers 
+#     def add(self, a, b=0, c=0): 
+#         return a + b + c 
  
-# Testing method overloading 
+# # Testing method overloading 
+# math_op = MathOperations() 
+# print("Addition of two numbers:", math_op.add(5, 10))   
+# print("Addition of three numbers:", math_op.add(5, 10, 15))   
+
+#Example of Method Overloading Using *args
+class MathOperations: 
+    def add(self, *args): 
+        return sum(args) 
+ 
+# Testing method overloading with varying arguments 
 math_op = MathOperations() 
 print("Addition of two numbers:", math_op.add(5, 10))   
-print("Addition of three numbers:", math_op.add(5, 10, 15))   
+print("Addition of three numbers:", math_op.add(5, 10, 15))  
+print("Addition of four numbers:", math_op.add(5, 10, 15, 20)) 
