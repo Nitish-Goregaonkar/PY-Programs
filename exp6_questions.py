7d0b81e8e56514dacef2e2fddfe194276be97e84
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-10 22:36:54 +0530

    py

--- exp6_questions.py
+++ exp6_questions.py
@@ -6,36 +6,3 @@ print("Outer tuple:", outer_tuple)
 inner_tuple = outer_tuple[1]
 print("Second inner tuple:", inner_tuple)
 # Accessing an element from an inner tuple
-element = outer_tuple[2][0]
-print("First element of the third inner tuple:", element)
-
-
-
-# Creating two tuples
-tuple1 = (5, 2, 9, 1)
-tuple2 = (8, 6, 4, 3)
-
-# Concatenating tuples
-concatenated_tuple = tuple1 + tuple2
-print("Concatenated tuple:", concatenated_tuple)
-
-# Sorting in ascending order
-sorted_ascending = tuple(sorted(concatenated_tuple))
-print("Sorted in ascending order:", sorted_ascending)
-
-# Sorting in descending order
-sorted_descending = tuple(sorted(concatenated_tuple, reverse=True))
-print("Sorted in descending order:", sorted_descending)
-
-
-
-# Creating a tuple of three elements
-original_tuple = (10, 20, 30)
-
-# Tuple unpacking into variables
-a, b, c = original_tuple
-print(f"Original values: a = {a}, b = {b}, c = {c}")
-
-# Swapping values using tuple unpacking
-a, b = b, a
-print(f"After swapping: a = {a}, b = {b}")
