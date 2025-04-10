7717f90c25993651650e95978d6deb301c88851f
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-10 22:35:46 +0530

    py

--- exp6_questions.py
+++ exp6_questions.py
@@ -1,3 +1,5 @@
 # Creating a tuple containing other tuples
 outer_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
 # Accessing elements from the outer tuple
+print("Outer tuple:", outer_tuple)
+# Accessing an inner tuple
