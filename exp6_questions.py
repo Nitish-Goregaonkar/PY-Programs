8f14896b082cf67d4a4abd2f2c6275f7fec3913b
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-10 22:36:02 +0530

    py

--- exp6_questions.py
+++ exp6_questions.py
@@ -3,3 +3,4 @@ outer_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
 # Accessing elements from the outer tuple
 print("Outer tuple:", outer_tuple)
 # Accessing an inner tuple
+inner_tuple = outer_tuple[1]
