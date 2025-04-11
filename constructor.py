1b352ce537dde83fdcfcd07c97c41a752f7ad632
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-11 23:21:44 +0530

    py

--- constructor.py
+++ constructor.py
@@ -5,3 +5,22 @@ def main():
     print ("accessing via object: ")
     print('public Variable :',a.public_var)
     print()
+    # print('private variable',a.__private_var) if we uncomment this print then error will occur
+class A:
+    # public variable
+    public_var=0
+
+    #private Variable
+    __private_var=1
+
+    def _init_(self,str='welcome!'):
+        print('constructor is started')
+        print ()
+        print("constructor(): ",str)
+        print()
+        print("public variable: ", self.public_var)
+        print()
+        print("private variable:",self.__private_var)
+
+#driver code
+main()
