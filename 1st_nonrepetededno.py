bb9a033e4d9bd2b347145587fc436dba4834b8cc
MarathaDada <goregaonkarnitish526@gmail.com>
2025-06-02 15:49:37 +0530

    programs new

---
+++ 1st_nonrepetededno.py
@@ -0,0 +1,9 @@
+def first_non_repeating_character(s):
+    for i in s:
+        if s.count(i) == 1:
+            return i
+    return None
+
+# Example usage:
+print(first_non_repeating_character("swiss"))
+
