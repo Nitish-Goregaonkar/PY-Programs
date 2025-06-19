3cfaa85a2eb9c25e6d2bc24e180a2e14f0c52e62
MarathaDada <goregaonkarnitish526@gmail.com>
2025-06-19 12:01:21 +0530

    update

--- OPERATOR_PRE.py
+++ OPERATOR_PRE.py
@@ -40,7 +40,6 @@ print("11. Bitwise OR:", 5 | 3)
 print("12. Comparison, Membership, Identity:", 5 > 3, 5 == 5, 3 in [1, 2, 3], 5 is not 4)
 
 
-
 # 16. Conditional Expression (Right-to-left)
 print("16. Conditional Expression:", "Yes" if 5 > 3 else "No")
 
