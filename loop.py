b7b260124ba9cd09aadf2093eb7a34bb7364ca7e
MarathaDada <goregaonkarnitish526@gmail.com>
2025-06-20 11:30:37 +0530

    update

--- loop.py
+++ loop.py
@@ -31,7 +31,6 @@ for u in range(1, 6):
           print(f"{u} x {x} = {u * x}", end='\t') 
      print()   
 
-
 #printing traingle program
 def pascals_triangle(n):
      triangle = [[1] * (i + 1) for i in range(n)]
