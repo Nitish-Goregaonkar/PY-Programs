7c6e693b4b2a5f43ef37107a4dd2773a021325be
MarathaDada <goregaonkarnitish526@gmail.com>
2025-06-20 11:30:48 +0530

    update

--- loop.py
+++ loop.py
@@ -24,7 +24,6 @@ while i <= rows:
        print()  # New line after each row 
 i += 1
            
-
 # Example: Nested for loop to print multiplication table 
 for u in range(1, 6): 
      for x in range(1, 6): 
